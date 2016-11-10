8e1d94b1688f282c6f11908f79f2ad71a4d924de
Johnny Hauser <johnnyhauser@gmail.com>
2016-11-09 19:35:20 -0600

    refactor

--- index.js
+++ index.js
@@ -1,39 +1,46 @@
 var concurrent = require('../concurrent-clean')
 var isNil = require('is-nil')
 
+function collect (fn) {
+  var args = []
+  fn(args.push.bind(args))
+  return args
+}
+
 function stringReplacement (string, pattern, replacement, cb) {
-  var result = string.replace(pattern, replacement)
-  cb(undefined, result)
+  // pass through `concurrent` so that the cb is async
+  concurrent([function (_cb) {
+    var result = string.replace(pattern, replacement)
+    cb(undefined, result)
+  }], function (errors, results) {
+    cb(errors, results[0])
+  })
+}
+
+function functionReplacement (string, pattern, replacer, cb) {
+  var setsOfArgs = collect(function (push) {
+    string.replace(pattern, function (match, string) {
+      push([].slice.call(arguments))
+    })
+  })
+  concurrent(setsOfArgs.map(function (args) {
+    return function (_cb) {
+      return replacer.apply(undefined, [_cb].concat(args))
+    }
+  }), function (errors, results) {
+    if (!isNil(errors)) {
+      return cb(errors)
+    }
+    var result = string.replace(pattern, function () {
+      return results.shift()
+    })
+    cb(undefined, result)
+  })
 }
 
 var replace = function (string, pattern, replacement, cb) {
-  var functions = []
-  if (typeof replacement === 'function') {
-    var replacer = replacement
-    var setsOfArgs = []
-    string.replace(pattern, function (match, string) {
-      setsOfArgs.push([].slice.call(arguments))
-    })
-    concurrent(setsOfArgs.map(function (args, idx) {
-      return function (_cb) {
-        return replacer.apply(undefined, [_cb].concat(args))
-      }
-    }), function (errors, results) {
-      if (!isNil(errors)) {
-        return cb(errors)
-      }
-      var result = string.replace(pattern, function () {
-        return results.shift()
-      })
-      cb(undefined, result)
-    })
-  } else {
-    concurrent([function (_cb) {
-      stringReplacement(string, pattern, replacement, _cb)
-    }], function (errors, results) {
-      cb(errors, results[0])
-    })
-  }
+  var fn = typeof replacement === 'function' ? functionReplacement : stringReplacement
+  fn(string, pattern, replacement, cb)
 }
 
 module.exports = replace
